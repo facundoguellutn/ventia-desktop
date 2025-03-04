9329dbd359ceeb2eac5d0432def75e85769c73a8
facundoelcapo <79200@sistemas.frc.utn.edu.ar>
2025-03-04 14:17:10 -0300

    Agregar componentes Shadcn y configuración de estilos

--- ventia-client/src/renderer/src/App.tsx
+++ ventia-client/src/renderer/src/App.tsx
@@ -1,11 +1,13 @@
+import { Button } from "./components/ui/button"
 
 
 function App(): JSX.Element {
 
   return (
-    <>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-2xl font-bold text-blue-500">Ventia</h1>
-    </>
+      <Button variant="outline">Click me</Button>
+    </div>
   )
 }
 
